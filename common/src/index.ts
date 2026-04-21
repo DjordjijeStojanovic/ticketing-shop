923ffca3a8b63832cf1c9346342e3d3d171573f3
Djordje <korisnik@Korisniks-MacBook-Pro.local>
2026-04-21 11:50:16 +0200

    Bump

---
+++ common/src/index.ts
@@ -0,0 +1,13 @@
+interface Drakula {
+    teeth: boolean;
+    scary: string;
+}
+
+const drakula: Drakula = {
+    teeth:true,
+    scary: ''
+};
+
+console.log(drakula);
+
+export default drakula;
