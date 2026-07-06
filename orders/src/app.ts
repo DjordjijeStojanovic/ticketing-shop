04814f57cb2a02a8cb555e397c011902915f66a3
Djordje <korisnik@Korisniks-MacBook-Pro.local>
2026-07-06 14:24:11 +0200

    Update hostname for Ingress

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -18,7 +18,7 @@ app.set('trust proxy', true);
 app.use(express.json());
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV === 'production'
+    secure: false
 }));
 
 app.use(currentUser);
